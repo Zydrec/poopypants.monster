69f25d655185e59e240397661dcf50b6b04ea9e0
Zydrec <vapredzn@gmail.com>
2025-10-31 18:10:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const images = [
   "images/image1.jpg",
   "images/image2.jpg",
   "images/image3.jpg",
-  "images/image4.jpg"
-  "images/image5.jpg"
-  "images/image6.jpg"
-  "images/image7.jpg"
-  "images/image8.webp"
+  "images/image4.jpg",
+  "images/image5.jpg",
+  "images/image6.jpg",
+  "images/image7.jpg",
+  "images/image8.webp",
 ];
 const randomImage = images[Math.floor(Math.random() * images.length)];
 document.getElementById("mainImage").src = randomImage;
